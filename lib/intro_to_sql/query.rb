438825dc3373107c1f0b0b45e595c94298d187a8
Dakota L Martinez <dakotaleemusic@gmail.com>
2021-06-11 11:41:57 -0700

    finish specs

--- lib/intro_to_sql/query.rb
+++ lib/intro_to_sql/query.rb
@@ -1,9 +1,12 @@
 class Query
   DB = SQLite3::Database.new("db/Chinook_Sqlite.sqlite", results_as_hash: true)
 
-
   def self.run(query)
     query.is_a?(Symbol) ? DB.execute(self.send(query)) : DB.execute(query)
+  end
+
+  def self.your_name
+    @@name = "Dakota" # change this to your name so the specs can read it!
   end
 
   # write a query that returns all the artists in the database
@@ -30,35 +33,35 @@ class Query
   # write the SQL to alter the fans table to have a artist_id column type integer?
   def self.add_artist_id_to_fans
     <<-SQL
-      ALTER TABLE fans ADD COLUMN artist_id INTEGER
+      
     SQL
   end
 
   # Write the SQL to add yourself as a fan of the Black Eyed Peas? ArtistId **169**
   def self.make_yourself_a_fan_of_the_black_eyed_peas
     <<-SQL
-
+      
     SQL
   end
 
   # write a SQL query to update your name in the fans table
   def self.update_your_name_in_fans
     <<-SQL
-
+      
     SQL
   end
 
   # write the SQL to display an artists name next to their album title
   def self.select_artist_name_and_album_title
     <<-SQL
-
+      
     SQL
   end
 
   # write the SQL to display artist name, album name and number of tracks on that album
   def self.select_artist_name_album_name_and_track_count
     <<-SQL
-
+      
     SQL
   end
 
--- spec/01_query_spec.rb
+++ spec/01_query_spec.rb
@@ -1,7 +1,11 @@
 RSpec.describe "Query" do 
+  before(:all) do 
+    Query::DB.execute("DROP TABLE IF EXISTS fans")
+  end
+  @@your_name = "Dakota"
   describe ".run(query)" do 
     it "takes a query as an argument and executes the query on the database object" do 
-      db = Query.class_variable_get("@@db")
+      db = Query::DB
       expect(db).to receive(:execute).with("SELECT * FROM Artist")
       Query.run("SELECT * FROM Artist")
     end
@@ -37,7 +41,57 @@ RSpec.describe "Query" do
     it "adds an artist_id integer foreign key to the fans table" do 
       column_names = Query.run("PRAGMA table_info(fans)").map{|col_data| col_data["name"]}
       Query.run(:add_artist_id_to_fans) unless column_names.include?("artist_id")
-      expect(column_names).to include("artist_id")
+      updated_column_names = Query.run("PRAGMA table_info(fans)").map{|col_data| col_data["name"]}
+      expect(updated_column_names).to include("artist_id")
+    end
+  end
+
+  describe ".make_yourself_a_fan_of_the_black_eyed_peas" do 
+    it "adds your name to the fans table as a fan of the black eyed peas (ArtistId **169**)" do 
+      Query.run(:make_yourself_a_fan_of_the_black_eyed_peas)
+      black_eyed_peas_fans = Query.run("SELECT * FROM fans WHERE artist_id = 169").map{|row| row["name"]}
+      expect(black_eyed_peas_fans).to include(Query.your_name)
+
+    end
+
+  end
+
+  describe ".update_your_name_in_fans" do 
+    it "updates the name of the fan you just created(Query.your_name) to a new name" do 
+      your_fan_id = Query.run("SELECT id FROM fans WHERE name = '#{Query.your_name}'").last["id"]
+      Query.run(:update_your_name_in_fans)
+      expect(Query.run("SELECT name FROM fans WHERE id = #{your_fan_id}").first["name"]).not_to eq(Query.your_name)
+    end
+  end
+
+  describe "select_artist_name_and_album_title" do 
+    it "returns an array of hashes containing album titles and the album artist's name HINT: need a JOIN and an AS clause for this" do 
+      result = Query.run(:select_artist_name_and_album_title)
+      expect(result.first).to eq({
+        "artist_name" => "AC/DC",
+        "album_title" => "For Those About To Rock We Salute You"
+      })
+      expect(result.last).to eq({
+        "artist_name" => "Philip Glass Ensemble",
+        "album_title" => "Koyaanisqatsi (Soundtrack from the Motion Picture)"
+      })
+    end
+  end
+  
+  describe "select_artist_name_album_name_and_track_count" do 
+    it "returns an array of hashes containing the artist's name, album_name and track_count for the album HINT: data spread over 3 tables, so you'll need multiple join statements and you also need to COUNT all tracks associated with an album. Because COUNT is an aggregate function, you may need to group first" do 
+      puts "ANOTHER HINT: https://www.w3schools.com/sql/sql_groupby.asp could be helpful"
+      results = Query.run(:select_artist_name_album_name_and_track_count)
+      expect(results.first).to eq({
+        "artist_name" => "AC/DC",
+        "album_title" => "For Those About To Rock We Salute You",
+        "track_count" => 10
+      })
+      expect(results.last).to eq({
+        "artist_name" => "Philip Glass Ensemble",
+        "album_title" => "Koyaanisqatsi (Soundtrack from the Motion Picture)",
+        "track_count" => 1
+      })
     end
   end
 
